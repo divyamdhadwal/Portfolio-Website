ad1780d475a2cf84374c612a17267a020eaeed23
Divyam Dhadwal <divyamdhadwal@icloud.com>
2017-08-15 16:06:04 +0530

    First commit

---
+++ js/javascript.js
@@ -0,0 +1,37 @@
+function image1(){
+	document.getElementById("myimage").src="images/first.png";
+}
+
+function image2(){
+	document.getElementById("myimage").src="images/second.png";
+}
+
+function image3(){
+	document.getElementById("myimage").src="images/third.png";
+}
+
+function image4(){
+	document.getElementById("myimage").src="images/fourth.png";
+}
+
+function image5(){
+	document.getElementById("myimage").src="images/fifth.png";
+}
+
+function image6(){
+	document.getElementById("myimage").src="images/sixth.png";
+}
+
+function image7(){
+	document.getElementById("myimage").src="images/seventh.png";
+}
+
+function changecontent2and3(){
+	document.getElementById("cont2").innerHTML="Online Examination Portal was designed to create an interactive platform for conducting multi choice questions examination on a web server using a website. Project aims at creating an user-friendly administration panel, user panel and student panel.<br><a href='index.html'><img src='images/back.png' alt='Go Back' style='width:20%;'></a>";
+	document.getElementById("cont3").innerHTML="<strong>Admin Login Details</strong> :<br>Username: admin<br>Password: admin<br><strong>Student Login details</strong> :<br>Username: 1411003030149<br>Password: 28011997";
+}
+
+function changecontent1and2(){
+	document.getElementById("cont1").innerHTML="This is the official website of Innovate 2k17 - An organisation of SRM University which conducts technical events for a period of two days. The main aim was to design an informative website along with the an option for students to get registered on the website , register for the events they want to participate in, check event details, propose new events, check personal messages and other such features. Faculty members or committee members had the privilege to check total registrations , registrations for specific events, accept or reject proposals , assign event coordinators and so on. Event coordinators panel allowed the coordinators to update their event details, check list of participants and their details for their respective event, check the transaction status of an registration, check the validity of their requirements and much more.";
+	document.getElementById("cont2").innerHTML="<strong>Admin Login Details</strong> :<br>Reg No: RA1411003030186<br>EmailID: divyamdhadwal2004@gmail.com<br>Password: divyam2011rock<br><strong>Participant Login details</strong> :<br>Reg.No: RA1611003030367<br>EmailID: sharma.aruj7677@gmail.com<br>Password: sharma456j<br><a href='index.html'><img alt='Go Back' src='images/back.png' style='width:20%;'></a>";
+}
